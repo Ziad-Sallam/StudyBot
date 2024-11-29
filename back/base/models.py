1a2185cfd5bf4cb17519f69006da7aeace5d8248
lojinesameh850 <153772405+lojinesameh850@users.noreply.github.com>
2024-11-29 22:04:27 +0200

    Update models.py
    Assignment status table, Assignment & User table, Tasks table

--- back/base/models.py
+++ back/base/models.py
@@ -1,27 +1,33 @@
 from django.db import models
-from django.core.exceptions import ValidationError
 from django.contrib.auth.models import User
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 
 class Subject(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=40)
 
-    # Human-readable string representation
     def __str__(self):
         return self.name
-
-class Assignment(models.Model): 
-    subject_tag = models.ForeignKey(Subject, on_delete=models.CASCADE)  # On deleting a subject, the associated assignments will also be deleted
     
-    type_tag = models.ForeignKey("AssignmentType",on_delete=models.CASCADE)
-    custom_type = models.CharField(max_length=20, blank=True, null=True)
-    
-    # In case custom is chosen but not defined
-    def clean(self):
-        if self.type_tag == 'custom_assignment' and not self.custom_type:
-            raise ValidationError('Custom type must be specified when type is custom.')
+class Assignment(models.Model):
+    type = models.ForeignKey(
+        "AssignmentType",
+        on_delete=models.CASCADE,
+        default=1  # Replace `1` with an appropriate value from your database
+    )
 
-    # Return human-readable name for assignment type
-    def __str__(self):
-        return f"{self.get_type_tag_display()} - {self.subject_tag.name}"
 class AssignmentType(models.Model):
     type = models.CharField(max_length=20, unique=True)
+
+class AssignmentStatus(models.Model):
+    status = models.CharField(max_length=30)
+
+class UserAssignment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  # Referencing built-in User model
+    assignment = models.ForeignKey("Assignment", on_delete=models.CASCADE)
+    status = models.ForeignKey("AssignmentStatus", on_delete=models.CASCADE)
+
+class Tasks(models.Model):
+    description = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  # Referencing built-in User model
+    flag = models.BooleanField()
+    
