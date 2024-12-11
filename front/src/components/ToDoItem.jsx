feb6d41f48330a9369ec573cd8280d436049ae39
Ziad-Sallam <ziadsallam1234@gmail.com>
2024-12-11 19:37:27 +0200

    login and home page

--- front/src/components/ToDoItem.jsx
+++ front/src/components/ToDoItem.jsx
@@ -1,4 +1,6 @@
 import PropTypes from "prop-types";
+import "../css/todo.css"
+import {useState} from "react";
 
 ToDoItem.propTypes ={
     title:PropTypes.string,
@@ -8,28 +10,40 @@ ToDoItem.propTypes ={
 
 
 function ToDoItem(props) {
+    const [on, setOn] = useState(false);
+
+    function onToggle() {
+        setOn(!on);
+    }
     return (
-        <tr className="list-group-item d-flex justify-content-between align-items-start">
-            <th>
-            <div className="ms-2 me-auto">
-                <div className="form-check form-check-inline">
-                    <input className="form-check-input me-1" type="checkbox" value=""
-                           id="firstCheckbox"/>
+        <div className={"todo-item " + (on? "todo-item-on" :"todo-item-off")} onClick={onToggle}>
+            <table className="list-group">
+            <tr className=" d-flex justify-content-between align-items-start">
+                <th>
+                    <div className="ms-2 me-auto">
+                        <div className="form-check form-check-inline">
+                            <input className="form-check-input me-1 check" type="checkbox" value=""
+                                   id="firstCheckbox"/>
 
-                    <div className="fw-bold">{props.title}</div>
+                            <div className="fw-bold text-white">{props.title}</div>
 
-                    <div className="fw-medium">{props.description}</div>
-                </div>
-            </div>
-            </th>
-            <th className="table-responsive-lg">
-                <div className="ms-2 me-auto">
-                    {props.date}
-                </div>
-            </th>
 
-        </tr>
+                        </div>
+                    </div>
+                </th>
+                <th className="table-responsive-lg">
+                    <div className="ms-2 me-auto text-white">
+                        {props.date}
+                    </div>
+                </th>
+
+            </tr>
+            </table>
+            {on && <div className="fw-medium text-white">{props.description}</div>}
+
+        </div>
     )
 
 }
+
 export default ToDoItem;
